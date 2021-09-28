b1e2c168a1b14ead92e1f540be73ca654e782db9
MOSDEV ALLY <sangaamosi04@gmail.com>
2021-09-28 13:26:04 +0300

    main.js added

---
+++ main.js
@@ -0,0 +1,54 @@
+//DOM element references
+const kilometers = document.getElementById("kilometers")
+const meters = document.getElementById("meters")
+
+kilometers.addEventListener( "focus", () => {
+    emptyValue(kilometers)
+} )
+meters.addEventListener( "focus", () => {
+    emptyValue(meters)
+} )
+
+function emptyValue(el) {
+   el.value = ""
+}
+
+const possibleError = "invalid number"
+
+kilometers.addEventListener( "input", kilometersToMeters )
+meters.addEventListener( "input", metersToKilometers )
+
+function kilometersToMeters() {
+    // return if input is NOT absolute value
+    const kilometersInput = kilometers.value
+
+    // do conversion KM to M
+    const metersInput = kilometersInput * 1000
+    // render on page
+    meters.value = `${metersInput} meters`
+
+    if(isNaN(kilometersInput)) {
+        meters.value = possibleError
+    }
+
+}
+
+
+function metersToKilometers() {
+    // return if input is NOT absolute value
+    const metersInput = meters.value
+
+    
+
+    // do conversion KM to M
+    const kilometersInput = metersInput / 1000
+    // render on page
+    kilometers.value = `${kilometersInput} kilometers`
+  
+    if(isNaN(metersInput)) {
+        kilometers.value = possibleError
+    }
+    
+}
+
+
